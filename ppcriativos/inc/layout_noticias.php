6976681256be0a38317781036130b1682548da28
vitaozim <victorhugocgodoi@gmail.com>
2016-11-01 12:57:52 -0200

    ppcriativos
    -interacao do cursor com os membros da equipe finalizado
    -botao que revela mais noticias funcionando
    -tirei animações de introdução

--- ppcriativos/inc/layout_noticias.php
+++ ppcriativos/inc/layout_noticias.php
@@ -4,7 +4,7 @@
 		<div class="ultimasnoticias">
 
 			<?php 
-			$args = array( 'post_type' => 'noticias', 'offset'=> 0, 'numberposts' => 3, 'order' => DESC); 
+			$args = array( 'post_type' => 'noticias', 'offset'=> 0, 'numberposts' => 99999, 'order' => DESC); 
 			$postslist = get_posts( $args );
 			foreach ( $postslist as $post ) : setup_postdata( $post ); 
 			?> 
@@ -25,7 +25,7 @@
 		</div>
 		<!-- Botão para ver o restante das notícias -->
 		<div class="cont-maisnoticias">
-			<a href="" class="maisnoticias">Mais notícias</a> 
+			<a class="maisnoticias">Mais notícias</a> 
 		</div>
 		</div>
 </div>
--- ppcriativos/js/script.js
+++ ppcriativos/js/script.js
@@ -3,23 +3,20 @@ jQuery(document).ready(function() {
 		contGrafismo = contIntro.find('div.grafismointro'),
 		grafismos = contGrafismo.find('div.posrel > img'),
 		logo_intro = jQuery('div#logointro'),
-		logosvg = logo_intro.children('svg'),
-		sublogointro = logo_intro.children('h3'),
-		letrasLogointro = logosvg.children('svg path'),
-		formaLogointro = logosvg.children('svg polygon'),
 		contEquipe = jQuery('div#equipe'),
 		wrapEquipe = contEquipe.find('div.cont-equipes'),
 		divsEquipe = wrapEquipe.children('div'),
 		widthCont = contIntro.width(),
 		heightCont = contIntro.height(),
 		equipeMargemPerspec = 30,
+		equipeMargemRotate = 20,
+		equipeMargemZ = 100,
 		// margemPerspec = 25,
 		intervaloGrafismos = 30,
 		intervaloSomado = 0,
-		// tempoLogo = intervaloGrafismos*grafismos.length+1500,
-		intervaloLetrasLogo = 20,
-		intervaloLetrasSomado = 0;
-		// tempoTrianguloLogo = tempoLogo + (intervaloLetrasLogo*letrasLogointro.length);
+		contNoticias = jQuery('div#noticias'),
+		conjNoticias = contNoticias.find('div.ultimasnoticias > div.noticia'),
+		btMaisNoticias = contNoticias.find('.cont-maisnoticias > a.maisnoticias');
 
 
 	// atualizando os valores de width e height do container dos grafismos
@@ -28,31 +25,6 @@ jQuery(document).ready(function() {
 		heightCont = contGrafismo.height();
 	});
 	
-	// animacao grafismos
-	grafismos.each(function(index, el) {
-		intervaloSomado += intervaloGrafismos;
-		setTimeout(function(){
-			jQuery(el).addClass('visivel');	
-		}, intervaloSomado);
-
-		// console.log(jQuery(el).attr('data-z-inicio'), jQuery(el).attr('data-z-final'),  Math.abs( jQuery(el).attr('data-z-final') - jQuery(el).attr('data-z-inicio') ));
-				
-	});
-
-// logo aparece
-	sublogointro.addClass('visivel');
-	formaLogointro.addClass('visivel');
-	letrasLogointro.each(function(index, el) {
-		intervaloLetrasSomado += intervaloLetrasLogo;
-		setTimeout(function(){
-			jQuery(el).addClass('visivel');
-		}, intervaloLetrasSomado);
-	});
-
-
-
-
-
 
 	contIntro.on('mousemove', function(event) {
 		var qtdeMov = (event.pageX+event.pageY)/(widthCont+heightCont);
@@ -86,11 +58,35 @@ jQuery(document).ready(function() {
 		var qtdeX = 50+(equipeMargemPerspec*(Math.round( (xcursor/jQuery(this).width()*2-1)*100)/100) ) ;
 		var qtdeY = 50+(equipeMargemPerspec*(Math.round( (ycursor/jQuery(this).height()*2-1)*100)/100) );
 
-		// console.log(qtdeX, qtdeY);
+		// console.log(xcursor, ycursor);
+		wrapEquipe.css('perspective-origin', qtdeX+'% '+qtdeY+'%');
+
 		divsEquipe.each(function(index, el) {
-			wrapEquipe.css('perspective-origin', qtdeX+'% '+qtdeY+'%');
+			var xel = Math.round( jQuery(el).offset().left + (jQuery(el).width()/2) );
+			var yel = Math.round( jQuery(el).offset().top + (jQuery(el).height()/2)  - contEquipe.offset().top );
+			
+
+			var qtde_transZ =equipeMargemZ - equipeMargemZ*( ( Math.sqrt( (xcursor-xel)*(xcursor-xel) + (ycursor-yel)*(ycursor-yel) ) ) / (Math.sqrt( contEquipe.width()*contEquipe.width() + contEquipe.height()*contEquipe.height() ) ) );
+		
+
+			jQuery(el).css('transform', 'translateZ('+qtde_transZ+'px)');
 		});
 	});
 
+	btMaisNoticias.on('click', function(event) {
+		jQuery(this).remove();
+		var intervaloNoticias = 60,
+			intNotSomado = 0;
+		conjNoticias.each(function(index, el) {
+			if (index > 2) {
+				jQuery(el).addClass('db');
+			}
+			setTimeout(function(){
+				jQuery(el).addClass('visivel');
+			}, intNotSomado);
+
+			intNotSomado += intervaloNoticias;
+		});
+	});
 
 });
